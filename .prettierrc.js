7c3188e3c14be0fd41ebcdd11d9e3274fcccd502
Trevor O'Brien <tmobrien55@gmail.com>
2023-04-02 23:37:09 -0700

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "bracketSpacing": true,
+    "jsxBracketSameLine": false,
+    "printWidth": 120,
+    "semi": false,
+    "singleQuote": true,
+    "tabWidth": 4,
+    "arrowParens": "avoid",
+    "trailingComma": "none" 
+}
